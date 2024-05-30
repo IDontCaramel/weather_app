d5dc3515e2b7741792783243e90d345feb16d9c2
jezz-07 <nextbookkids2@gmail.com>
2024-05-30 14:45:24 +0200

    oeialalla

--- weatherApp/Form1.cs
+++ weatherApp/Form1.cs
@@ -15,6 +15,13 @@ namespace weatherApp
         public Form1()
         {
             InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NederlandKaart form2 = new NederlandKaart();
+            form2.Show();
         }
     }
 }
