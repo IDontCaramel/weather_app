58c7b6159b0b80f72dc81d006fa55105841bc748
jezz-07 <nextbookkids2@gmail.com>
2024-05-30 12:49:22 +0200

    Map dev

--- weatherApp/Form1.cs
+++ weatherApp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,29 @@ namespace weatherApp
         public Form1()
         {
             InitializeComponent();
+            this.pictureBox1.MouseClick += new MouseEventHandler(this.pictureBox1_MouseClick);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            double geschaaldeX = SchaalNaarLandKaart(x, 705, 52.3295182);
+            double geschaaldeY = SchaalNaarLandKaart(y, 416, 4.9227935);
+
+            MessageBox.Show($"Geschaalde x-coördinaat: {geschaaldeX}\nGeschaalde y-coördinaat: {geschaaldeY}");
+        }
+
+        private double SchaalNaarLandKaart(int waarde, int oorspronkelijkeMax, double landMapMax)
+        {
+            double oorspronkelijkeBereik = oorspronkelijkeMax - 0; // Minimumwaarde is altijd 0 voor de PictureBox
+            double landMapBereik = landMapMax - 0;
+
+            // Schaal de waarde naar de landkaart
+            double geschaaldeWaarde = (waarde * landMapBereik) / oorspronkelijkeBereik;
+
+            return geschaaldeWaarde;
         }
     }
 }
